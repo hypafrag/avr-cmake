d213fb6ce0ce8b93f7f5125bbd71fe9cb97d9b28
escobar <escobar@mosk.ru>
2020-02-08 09:57:20 +0700

    buttons

--- src/init.c
+++ src/init.c
@@ -1,3 +1,5 @@
+#define __DELAY_BACKWARD_COMPATIBLE__
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
@@ -14,7 +16,7 @@ const uint8_t SegCode[] = {
 #define RefreshRate 45.0
 #define OnDelay (1000.0 / (RefreshRate * 3))
 
-void show(uint8_t v)
+void show(uint8_t v, double delay)
 {
 	uint8_t digit;
 
@@ -23,19 +25,19 @@ void show(uint8_t v)
 
 	PORTB = SegOne;
 	PORTD = SegCode[digit];
-	_delay_ms(OnDelay);
+	_delay_ms(delay);
 
 	digit = v / 10;
 
 	PORTB = SegTwo;
 	PORTD = SegCode[digit];
-	_delay_ms(OnDelay);
+	_delay_ms(delay);
 
 	digit = v % 10;
 
 	PORTB = SegThree;
 	PORTD = SegCode[digit];
-	_delay_ms(OnDelay);
+	_delay_ms(delay);
 }
 
 int main(void)
@@ -43,13 +45,25 @@ int main(void)
 	// Configure the ports as output
 	DDRB = 0xff; // Control signal PORTB0-PORTB2
 	DDRD = 0xff; // Data lines
+	DDRC = 0x01; // Blinker
+	PORTC = 0x06; // Button pull-up for C1 and C2
 	while (1)
 	{
 		for (uint8_t i = 0; i <= 255; i++)
 		{
 			for (uint8_t t = 0; t < (uint8_t)(RefreshRate); t++)
 			{
-				show(i);
+				double delay = OnDelay;
+				if (bit_is_clear(PINC, PC1))
+				{
+					delay *= 0.5;
+				}
+				if (bit_is_clear(PINC, PC2))
+				{
+					delay *= 2;
+				}
+				show(i, delay);
+				PORTC = ~PORTC;
 			}
 		}
 	}
